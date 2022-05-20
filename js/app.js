e76c5788a2deadd3765a7bb1505d6ecbf0e5a76b
Sara Rodriguez <s.rodriguez91s@hotmail.com>
2022-05-20 20:30:42 +0200

    ej acabados

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-// Implementar solución aquí
+// Implementar solución aquí 
+
+let colorRandom = document.querySelector("#btn");
+colorRandom.onclick = ponerColorRandom;
+
+
+function ponerColorRandom(){
+    let valorColor = generarColor();
+    document.body.style.backgroundColor = "#" + valorColor;
+    let etiquetaSpan = document.querySelector("#hex-value");
+    etiquetaSpan.innerHTML = "#" + valorColor;
+    
+
+}
+
+
+function generarColor() {
+    return Math.floor(Math.random()*16777215).toString(16);
+    
+}
+
+
 
